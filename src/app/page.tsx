f4bc95e92f7dc345c28c3e96ba87fcc955150d2e
Orbitadoo <juanpaavaldezz@gmail.com>
2024-12-14 20:49:50 -0300

    feat: added supabase and renderize it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
-export default function Home() {
-  return <h1>Home</h1>;
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+export default async function Home() {
+  const supabase = createServerComponentClient({ cookies });
+
+  const { data: venues } = await supabase.from("venues").select("*");
+
+  if (!venues) {
+    return <div>No data</div>;
+  }
+
+  return (
+    <>
+      {venues.map((venue) => (
+        <ul key={venue.description}>
+          <li>{venue.name}</li>
+          <li>{venue.description}</li>
+        </ul>
+      ))}
+    </>
+  );
 }
